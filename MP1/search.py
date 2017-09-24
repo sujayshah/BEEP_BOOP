5b6b43c3ffa48ce3f00d9d5d3f1005780cb449ae
Emma Tai <emmatai@Emmas-MacBook-Pro-2.local>
2017-09-23 21:35:16 -0500

    adding start function and comments

--- MP1/search.py
+++ MP1/search.py
@@ -1,21 +1,57 @@
+# global definition# 
+x_start = 0
+y_start = 0
+
+# This function reads in the maze from the text file and generates the 
+# 2D array to traverse through.  									   
 def read_map(mapname):
-	with open(mapname) as textFile:
-		lines = [line.strip().split('\r\n ') for line in textFile]
+	textFile = open(mapname, "r")
+	maze = []
 
-	return lines
+	for line in textFile:
+		maze.append(line.strip().split('\r\n '))
 
+	textFile.close()
+
+	return maze
+
+# This function writes back to a text file our 2D array that we read in. 
+# Namely used to verify we read in text file correctly.
 def write_test_map(mapname):
-	my_list = read_map("tinySearch.txt")
+	maze = read_map(mapname)
 
 	with open(mapname, 'w') as outFile:
 		for item in my_list:
 			outFile.write("%s\n"  %item)
 
+# This function finds the (x,y) start position of the maze, marked as 
+# 'P' in the text file.
+def find_start(mapname):
+	maze = read_map(mapname)
+	global x_start 
+	global y_start
+
+	for ypos, line in enumerate(maze):
+		for string in line:
+			for xpos, char in enumerate(string):
+				if char == 'P':
+					x_start = xpos
+					y_start = ypos 
+					#print xpos, ypos			
+# Main function
 def main():
 	lines2= read_map("tinySearch.txt")
-	print lines2
+	#write_test_map("tinySearch_test.txt")
 
-	write_test_map("tinySearch_test.txt")
+	for line in lines2:
+		for s in line: 
+			print s
+
+	find_start("tinySearch.txt")
+
+	#print "Main print: "
+	#print x_start 
+	#print y_start
 
 if __name__ == "__main__":
 	main()
