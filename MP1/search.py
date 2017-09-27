cb21c31112bbbac67113e1a32b20e2a29db292bf
Emma Tai <emmatai@wirelessprv-10-194-61-250.near.illinois.edu>
2017-09-27 12:59:17 -0500

    cleaning up & commenting code

--- MP1/search.py
+++ MP1/search.py
@@ -68,10 +68,12 @@ def heuristic(point1, point2):
 
 	return manhattan_distance
 
-# This function conducts a BFS search of the maze. #
+# This function conducts a BFS search of the maze. Returns pointer to 
+# last solution node and total step cost as a tuple (node, cost).
 def bfs_search(mapname): 
 	maze = read_map(mapname)
 
+	cost = 0 
 	node = Node(x_start, y_start, None)
 	cur_x = x_start
 	cur_y = y_start
@@ -88,7 +90,7 @@ def bfs_search(mapname):
 	explored = {}
 	while 1:
 		if len(frontier) == 0:
-			return None
+			return (None, cost)
 
 		node = frontier.popleft()
 		nodenode = frontiernode.popleft()
@@ -104,10 +106,10 @@ def bfs_search(mapname):
 			child.parent = nodenode
 			if (child.x, child.y) not in explored and (child.x, child.y) not in frontier:	
 				if (child.x == x_end and child.y == y_end):
-					return child
+					return (child, cost)
 				frontier.append((child.x, child.y))
 				frontiernode.append(child)
-				print "Adding: " + str(child.x) + ", " + str(child.y)
+				#print "Adding: " + str(child.x) + ", " + str(child.y)
 
 		#if can move down, move down
 		if cur_y < down_bound and maze[cur_y + 1][0][cur_x]!= '%': 
@@ -115,10 +117,10 @@ def bfs_search(mapname):
 			child.parent = nodenode
 			if (child.x, child.y) not in explored and (child.x, child.y) not in frontier:
 				if (child.x == x_end and child.y == y_end):
-					return child
+					return (child, cost)
 				frontier.append((child.x, child.y))
 				frontiernode.append(child)
-				print "Adding: " + str(child.x) + ", " + str(child.y)
+				#print "Adding: " + str(child.x) + ", " + str(child.y)
 
 		#if can move up, move up
 		if cur_y > 0 and maze[cur_y - 1][0][cur_x]!= '%':
@@ -126,10 +128,10 @@ def bfs_search(mapname):
 			child.parent = nodenode
 			if (child.x, child.y) not in explored and (child.x, child.y) not in frontier:
 				if (child.x == x_end and child.y == y_end):
-					return child
+					return (child, cost)
 				frontier.append((child.x, child.y))
 				frontiernode.append(child)
-				print "Adding: " + str(child.x) + ", " + str(child.y)
+				#print "Adding: " + str(child.x) + ", " + str(child.y)
 
 		#if can move left, move left
 		if cur_x > 0 and maze[cur_y][0][cur_x-1]!= '%': 
@@ -137,12 +139,14 @@ def bfs_search(mapname):
 			child.parent = nodenode
 			if (child.x, child.y) not in explored and (child.x, child.y) not in frontier:
 				if (child.x == x_end and child.y == y_end):
-					return child
+					return (child, cost)
 				frontier.append((child.x, child.y))
 				frontiernode.append(child)
-				print "Adding: " + str(child.x) + ", " + str(child.y)
-	return None
+				#print "Adding: " + str(child.x) + ", " + str(child.y)
+	return (None, cost)
 
+# This function conducts a dfs search of the maze. Returns pointer to 
+# last solution node and total step cost as a tuple (node, cost).
 def dfs_search(mapname):
 	maze = read_map(mapname)
 
@@ -213,17 +217,12 @@ def aStar_search(mapname):
 	if(cur_x == x_end and cur_y == y_end):
 		return node 
 
-
-
-
-
-
-
-
 def manhattan_dist(x1, x2, y1, y2):
 	dist = abs(x1 - x2) - (y1 - y2)
 	return dist
 
+# This function conducts a greedy best-first search of the maze. Returns pointer
+# to last solution node and total path cost as a tuple (node, cost). 
 def greedybfs_search(mapname):
 	maze = read_map(mapname)
 
@@ -343,22 +342,20 @@ def main(mapname):
 	
 	solution = []
 
-	temp = dfs_search(mapname)
-
-	#temp2 = greedybfs_search(mapname)
-	#temp = temp2[0]
-	#cost = temp2[1]
+	temp2 = bfs_search(mapname)
+	temp = temp2[0]
+	cost = temp2[1]
 	if temp != None: 
 		print 'SUCCESS!'
-		#print 'End located at: (' + str(temp.x ) + ", " + str(temp.y)  + ")"
+		print 'End located at: (' + str(temp.x ) + ", " + str(temp.y)  + ")"
 
 		while(temp != None):
-			print temp.x, temp.y
+			#print temp.x, temp.y
 			solution.append ((temp.x, temp.y))
 			temp = temp.parent
 
 	#print solution
-	#print "COST:" + str(cost)
+	print "COST: " + str(cost)
 	draw_solution(mapname, solution)
 	print "Solution drawn to " + mapname[:-4] + "test.txt"
 
