714b298f38801332990248d2b615edc12a1e57ec
Emma Tai <emmatai@Emmas-MacBook-Pro-2.local>
2017-09-28 20:49:40 -0500

    editing state field for node

--- MP1/node.py
+++ MP1/node.py
@@ -1,8 +1,8 @@
 class Node:
 
-	def __init__(self, xpos, ypos, action):
+	def __init__(self, xpos, ypos, instate):
 		self.x = xpos
 		self.y = ypos 
-		self.action = action
+		self.action = None
 		self.parent = None
-		self.state = None
+		self.state = instate
