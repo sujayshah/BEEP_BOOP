d9c5bfb62bf08418e22d2b855cc1a2b8940fd128
sujayshah <svshah047@gmail.com>
2017-10-19 16:20:33 -0500

    Still working

--- MP2/flowfree.py
+++ MP2/flowfree.py
@@ -14,50 +14,58 @@ def readFile(filename):
 
 def dumb_csp_search(grid):
 
+	# assignmentList = ["orange", "green", ]
 
-def smart_csp_search(grid):
-	
+	for space in grid:
+		# print space.x, space.y, space.assignment, space.domain, space.state
+		if space.state == 0:
+			for newAssign in space.domain:
+				space.assignment = newAssign
+				space.state = 2
+				if(space.x > 0 and getVar(grid, x-1, y)) and s
+		
+		print space.x, space.y, space.assignment, space.domain, space.state
+
+
+def getVar(grid, x, y):
+	for space in grid:
+		if (space.x == x and space.y == y):
+			return space
+
+	return None
 
 def main(filename):
 	flowFree = readFile(filename)
+	colorList = []
 	grid = []
 	x = 0
 	for line in flowFree:
 		for s in line: 
 			print s
 			for c in range(0,len(s)):
-				if s[c] == 'O':
-					var = Variable(x, c, s[c], None)
-					var.domain.append("orange")
-				elif s[c] == 'G':
-					var = Variable(x, c, s[c], None)
-					var.domain.append("green")
-				elif s[c] == 'R':
-					var = Variable(x, c, s[c], None)
-					var.domain.append("red")
-				elif s[c] == 'B':
-					var = Variable(x, c, s[c], None)
-					var.domain.append("blue")
-				elif s[c] == 'Y':
-					var = Variable(x, c, s[c], None)
-					var.domain.append("yellow")
+				if s[c] not in colorList and s[c] != '_':
+					colorList.append(s[c])
+				
+				if s[c] == '_':
+					var = Variable(x, c, s[c], None, None)
+					var.domain = colorList
+					var.state = 0
+
 				else:
-					var = Variable(x, c, s[c], None)
-					var.domain.append("orange")
-					var.domain.append("green")
-					var.domain.append("red")
-					var.domain.append("blue")
-					var.domain.append("yellow")
+					var = Variable(x, c, s[c], None, None)
+					var.domain.append(s[c])
+					var.state = 1
+
 				
 				grid.append(var)
 				
 			x+=1
 				
-	for space in grid:
-		print space.x, space.y, space.assignment, space.domain
+	# for space in grid:
+	# 	print space.x, space.y, space.assignment, space.domain
 
 	dumb_csp_search(grid)
 	# smart_csp_search(grid)
 
 if __name__ == "__main__" : 
-	main("input77.txt")
+	main("input88.txt")
