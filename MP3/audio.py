9b89fa4af7579532f9baa689d28edfd67d256456
Emma Tai <emmatai2@illinois.edu>
2017-11-23 17:25:24 -0600

    frequency tables done for yes and no

--- MP3/audio.py
+++ MP3/audio.py
@@ -8,15 +8,16 @@ indiv_low_list = []
 # Spectrograms are split into lines of 25 x 10 characters, seperated by 3 blank lines
 # 0 - 24 first spectrogram
 # 28 - 52 second spectrogram
+# 131 samples for no_train.txt
 def read_file(filename):
 	f = open(filename)
 	clear_file('temp.txt')
 	n = 0 
 	num_samples = 1 
 	lines = f.readlines() 
-	#while n < 3665:
-	charlist = [0] * 250 ## need to update to global frequency table
-	while n < 25:
+	charlist = [(0,0)] * 250 #frequency table. (low, high) = ('%', ' ')
+	
+	while n < 3665:
 		for idx, i in enumerate(range(n, n + 25)): #this is the loop that we can do our work in
 			write_file('temp.txt', lines[i])
 
@@ -24,29 +25,27 @@ def read_file(filename):
 				if col >= 10: #break on col index 10 since this is just the newline character leftover from readlines()
 					break
 
-				print "Looking at : " + str(idx * 10 + col), 
-
-				if char == '%':
-					print char, 
-					charlist[idx * 10 + col] = 0
-					print "Set!"
+				if char == '%': #low energy
+					#print char
+					lowprev = charlist[idx * 10 + col][0]
+					highprev = charlist[idx * 10 + col][1]
+					charlist[idx * 10 + col] = (lowprev + 1, highprev)
 				else: 
-					print char
-					charlist[idx * 10 + col] = 1
-					print "Set!"
-				
-			#print i
-		
+					#print char
+					lowprev = charlist[idx * 10 + col][0]
+					highprev = charlist[idx * 10 + col][1]
+					charlist[idx * 10 + col] = (lowprev, highprev + 1)
+			
 		#n = n + 24 + 4
-		write_file('temp.txt', lines[n + 25])
+		write_file('temp.txt', lines[n + 25]) #blank lines to separate
 		write_file('temp.txt', lines[n + 26])
 		write_file('temp.txt', lines[n + 27])
 
-		n = n + 24 + 4
+		n = n + 24 + 4 #start reading next sample
 		num_samples = num_samples + 1
-		print charlist
 
-	#print num_samples
+	#print charlist
+	return charlist
 
 
 # This function takes in a file and a line to write and appends the line to the file
@@ -67,7 +66,9 @@ def clear_file(filename):
 # P(W(i, j) = f| class)) = (# times pixel(i, j) has value f in training examples for this class) /
 # (Total # of training examples from this class)
 def main(samplename): 
-	read_file(samplename)
+	no_list = read_file('no_train.txt')
+	yes_list = read_file('yes_train.txt')
+	print no_list
 
 if __name__ == '__main__':
-	main('no_train.txt')
+	main('yes_train.txt')
