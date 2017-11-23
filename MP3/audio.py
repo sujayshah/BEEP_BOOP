19e4023c5d0cdebea3cf26a7e8570b2a2be91b86
Emma Tai <emmatai2@illinois.edu>
2017-11-23 01:30:57 -0600

    parsing input

--- MP3/audio.py
+++ MP3/audio.py
@@ -1,6 +1,7 @@
+from itertools import islice #islice is used to get the next n items of an iterator
 
 # This function reads in the text file
-def read_file(filename):
+def read_file1(filename):
 	textFile = open(filename, "r")
 	text = []
 
@@ -11,4 +12,36 @@ def read_file(filename):
 
 	return text
 
-# This
+def read_file(filename):
+	# 0 - 24 first spectogram
+	# 28 - 52 
+	n = 25
+	with open(filename, 'r') as infile: 
+		while True: 
+			next_n_lines = list(islice(infile, n))
+			if not next_n_lines:
+				break
+
+			for idx, line in enumerate(next_n_lines): 
+				print line
+				write_file('temp.txt', str(line))
+
+def write_file(filename, line):
+	with open(filename, 'a') as outFile:
+			outFile.write("%s\n" %line)
+	
+
+# every spectogram is described by 250 binary variables
+# W(i, j) = 1 if spectrogram(i, j) = ' ' (high energy)
+# W(i, j) = 0 if spectrogram(i, j) = '%' (low energy)
+# estimate the likelihoods P(W(i,j)= 1 | class) as proportion of the training data that is high energy
+
+# the goal of the training stage is to estimate the likelihoods P(W(i, j) | class) for 
+# every pixel location 
+# P(W(i, j) = f| class)) = (# times pixel(i, j) has value f in training examples for this class) /
+# (Total # of training examples from this class)
+def main(samplename): 
+	read_file(samplename)
+
+if __name__ == '__main__':
+	main('no_train.txt')
