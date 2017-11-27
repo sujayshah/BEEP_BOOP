6fd84d338e6e49698a8565e065d6c2822986f070
Emma Tai <emmatai2@illinois.edu>
2017-11-27 16:19:54 -0600

    counting likelihoods

--- MP3/audio.py
+++ MP3/audio.py
@@ -8,7 +8,8 @@ indiv_low_list = []
 # Spectrograms are split into lines of 25 x 10 characters, seperated by 3 blank lines
 # 0 - 24 first spectrogram
 # 28 - 52 second spectrogram
-# 131 samples for no_train.txt
+# 131 samples for no_train.txt, 131 samples for yes_train.txt
+# total 262 samples 
 def read_file(filename):
 	f = open(filename)
 	clear_file('temp.txt')
@@ -56,6 +57,24 @@ def write_file(filename, line):
 def clear_file(filename):
 	open(filename, 'w').close()
 
+def train(k): #k value is the value for Laplacian smoothing. 
+	no_list = read_file('no_train.txt')
+	yes_list = read_file('yes_train.txt')
+
+	likelihood_no = [0] * 250 # times location (i, j) has value '' in training examples from class NO / total # of training examples from this class
+	likelihood_yes = [0] * 250  # times location (i, j) has value '' in training examples from class YES/ total # of training examples from this class
+
+	for idx, sample in enumerate(no_list): 
+		likelihood_no[idx] = no_list[idx][1]/131.0
+
+	for idx, sample in enumerate(yes_list): 
+	 	likelihood_yes[idx] = yes_list[idx][1]/131.0
+
+	print no_list 
+	print likelihood_no
+	#print yes_list
+
+
 # every spectogram is described by 250 binary variables
 # W(i, j) = 1 if spectrogram(i, j) = ' ' (high energy)
 # W(i, j) = 0 if spectrogram(i, j) = '%' (low energy)
@@ -66,9 +85,7 @@ def clear_file(filename):
 # P(W(i, j) = f| class)) = (# times pixel(i, j) has value f in training examples for this class) /
 # (Total # of training examples from this class)
 def main(samplename): 
-	no_list = read_file('no_train.txt')
-	yes_list = read_file('yes_train.txt')
-	print no_list
+	train(0) #default WITHOUT LAPLACIAN SMOOTHING
 
 if __name__ == '__main__':
 	main('yes_train.txt')
