1cf6b227d235239f5c94679e1f79de42e51ea405
Emma Tai <emmatai2@illinois.edu>
2017-11-27 20:49:11 -0600

    commented audio.py

--- MP3/audio.py
+++ MP3/audio.py
@@ -15,7 +15,7 @@ def read_file(filename):
 	lines = f.readlines() 
 	charlist = [(0,0)] * 250 #frequency table. (low, high) = ('%', ' ')
 	
-	while n < 3665:
+	while n < 3665: #this is a constant found by printing out n (number of lines) and finding where we go out of bounds
 		for idx, i in enumerate(range(n, n + 25)): #this is the loop that we can do our work in
 			write_file('temp.txt', lines[i])
 
@@ -25,9 +25,9 @@ def read_file(filename):
 
 				if char == '%': #low energy
 					#print char
-					lowprev = charlist[idx * 10 + col][0]
+					lowprev = charlist[idx * 10 + col][0] # need to save the previous values since you can't edit tuple values in python
 					highprev = charlist[idx * 10 + col][1]
-					charlist[idx * 10 + col] = (lowprev + 1, highprev)
+					charlist[idx * 10 + col] = (lowprev + 1, highprev) # increase the count
 				else: 
 					#print char
 					lowprev = charlist[idx * 10 + col][0]
@@ -54,6 +54,16 @@ def write_file(filename, line):
 def clear_file(filename):
 	open(filename, 'w').close()
 
+# the goal of the training stage is to estimate the likelihoods P(W(i, j) | class) for 
+# every pixel location 
+# P(W(i, j) = f| class)) = (# times pixel(i, j) has value f in training examples for this class) /
+# (Total # of training examples from this class)
+
+# every spectogram is described by 250 binary variables
+# W(i, j) = 1 if spectrogram(i, j) = ' ' (high energy)
+# W(i, j) = 0 if spectrogram(i, j) = '%' (low energy)
+# estimate the likelihoods P(W(i,j)= 1 | class) as proportion of the training data that is high energy
+
 def train(k): #k value is the value for Laplacian smoothing. 
 	no_train = read_file('no_train.txt')
 	yes_train = read_file('yes_train.txt')
@@ -71,11 +81,9 @@ def train(k): #k value is the value for Laplacian smoothing.
 
 
 # This function performs a MAP classification of "yes" or "no" according to the learned model
-def test(filename, k):
+def test(filename, k, outputfile, likelihood_no, likelihood_yes):
 	f = open(filename)
-	clear_file('temp1.txt')
-	clear_file('likelihood_no.txt')
-	clear_file('likelihood_yes.txt')
+	clear_file(outputfile)
 
 	n = 0 
 	num_samples = 1 
@@ -83,12 +91,10 @@ def test(filename, k):
 	charlist = [(0,0)] * 250 #tuple of (P(location|NO), P(location|YES))
 	testlist = []
 	
-	likelihood_no = train(k)[0]
-	likelihood_yes= train(k)[1]
-	write_file('likelihood_no.txt', likelihood_no)
-	write_file('likelihood_yes.txt', likelihood_yes)
+	# likelihood_no = train(k)[0]
+	# likelihood_yes= train(k)[1]
 
-	testlines = []
+	#testlines = []
 	
 	while n < 1397:
 		# testlines.append(lines[n]) - this will print a growing list of the same sample
@@ -99,7 +105,7 @@ def test(filename, k):
 				if col >= 10: #break on col index 10 since this is just the newline character leftover from readlines()
 					break
 
-				if char == ' ': #high energy
+				if char == ' ': #high energy; keep a tuple of (P(W(i,j)| "NO"), P(W(i, j)|"YES"))
 					charlist[idx * 10 + col] = (log10(likelihood_no[idx * 10 + col]), log10(likelihood_yes[idx * 10 + col]))
 	
 				else: 
@@ -108,40 +114,30 @@ def test(filename, k):
 		label_no= log10(0.5)
 		label_yes = log10(0.5)
 
+		# calculate the values for MAP decision
 		for idx, probs in enumerate(charlist):
 			label_no = label_no + charlist[idx][0]
 			label_yes = label_yes + charlist[idx][1]
 
 		if label_no > label_yes: 
-			write_file("temp1.txt", "NO\n")
+			write_file(outputfile, "NO\n")
 		else:
-			write_file("temp1.txt", "YES\n")
+			write_file(outputfile, "YES\n")
 		#write_file('temp1.txt', testlines) - WORKING
 		#del testlines[:] - WORKING
-		#write_file('temp1.txt', charlist)
+
+		#clear charlist so we can use it again
 		del charlist[:]
 		charlist = [(0, 0)] * 250
 
-		# write_file('temp1.txt', lines[n + 25]) #blank lines to separate
-		# write_file('temp1.txt', lines[n + 26])
-		# write_file('temp1.txt', lines[n + 27])
-
 		n = n + 24 + 4 #start reading next sample
 		num_samples = num_samples + 1
-	
-# every spectogram is described by 250 binary variables
-# W(i, j) = 1 if spectrogram(i, j) = ' ' (high energy)
-# W(i, j) = 0 if spectrogram(i, j) = '%' (low energy)
-# estimate the likelihoods P(W(i,j)= 1 | class) as proportion of the training data that is high energy
 
-# the goal of the training stage is to estimate the likelihoods P(W(i, j) | class) for 
-# every pixel location 
-# P(W(i, j) = f| class)) = (# times pixel(i, j) has value f in training examples for this class) /
-# (Total # of training examples from this class)
 def main(samplename): 
-	#train(0) #default WITHOUT LAPLACIAN SMOOTHING
-	test("no_test.txt", 1)
-	#test("yes_test.txt")
+	likelihood_no = train(0.1)[0]
+	likelihood_yes = train(0.1)[1]
+	test("no_test.txt", 0.1, "no_results.txt", likelihood_no, likelihood_yes)
+	test("yes_test.txt", 0.1, "yes_results.txt", likelihood_no, likelihood_yes)
 
 if __name__ == '__main__':
 	main('yes_train.txt')
