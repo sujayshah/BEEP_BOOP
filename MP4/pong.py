5280ec371739149be30c7a6f2dfa4340f0ead23c
Emma Tai <emmatai2@illinois.edu>
2017-12-11 19:59:40 -0600

    fixing second loop

--- MP4/pong.py
+++ MP4/pong.py
@@ -2,42 +2,57 @@ import random
 from state import GameState as gameState
 import math
 
+#global variables 
+q_table = [list([0, 0, 0])] * 10369
+N= [list([0, 0, 0])] * 10369
+
 # This function udpates the ball position and checks the bounce/termination conditions. Returns a state
-def play_game(board): 
+def play_game(state, action): 
 	#initialize game
 	#check to make sure not terminal state first
-	if board == None or board.special == 1: 
+	if state == None or state.special == 1: 
 		print "TERMINAL STATE. GAME OVER"
 		return None
 
 	paddle_height = 0.2
 
+	state.paddle_y = state.paddle_y + state.actions[action]
+
+	#if paddle goes off the top of the screen
+	if state.paddle_y > 1:
+		state.paddle_y = 0
+
+	#if any part of the paddle goes off the bottom of the scr
+	if (state.paddle_y - paddle_height < 0):
+		state.paddle_y = 1 - paddle_height
+
+
 	#at each time step: 
 	#increment ball_x by velocity_x and ball_y by velocity_y 
-	orig_x = board.ball_x #old points 
-	orig_y = board.ball_y
+	orig_x = state.ball_x #old points 
+	orig_y = state.ball_y
 
-	board.ball_x = board.ball_x + board.velocity_x
-	board.ball_y = board.ball_y + board.velocity_y
+	state.ball_x = state.ball_x + state.velocity_x
+	state.ball_y = state.ball_y + state.velocity_y
 
-	new_x = board.ball_x #new points 
-	new_y = board.ball_y
+	new_x = state.ball_x #new points 
+	new_y = state.ball_y
 
 	#bounce
 	#if ball is off the top of the screen
-	if board.ball_y < 0: 
-		board.ball_y = -1 * board.ball_y
-		board.velocity_y = -1 * board.velocity_y 
+	if state.ball_y < 0: 
+		state.ball_y = -1 * state.ball_y
+		state.velocity_y = -1 * state.velocity_y 
 
 	#if ball is off bottom of screen 
-	if board.ball_y > 1: 
-		board.ball_y = 2 - board.ball_y 
-		board.velocity_y = -1 * board.velocity_y 
+	if state.ball_y > 1: 
+		state.ball_y = 2 - state.ball_y 
+		state.velocity_y = -1 * state.velocity_y 
 
 	#if ball is off left edge of screen 
-	if board.ball_x < 0: 
-		board.ball_x = -1 * board.ball_x 
-		board.velocity_x = -1 * board.velocity_x
+	if state.ball_x < 0: 
+		state.ball_x = -1 * state.ball_x 
+		state.velocity_x = -1 * state.velocity_x
 		if abs(board.velocity_x) <= 0.03:
 			print "VELOCITY_X ERROR"
 
@@ -46,24 +61,24 @@ def play_game(board):
 	slope = (new_y - orig_y)/(new_x - orig_x)
 	b = new_y - (slope * new_x)
 	y_intersection= slope + b # y = mx + b, plug in x = 1
- 	paddle_bottom = board.paddle_y - paddle_height
+ 	paddle_bottom = state.paddle_y - paddle_height
 
 	#if x>1 and line intersects the x=1 line within paddle range
-	if board.ball_x > 1 and (board.ball_y <= board.paddle_y and board.ball_y >= paddle_bottom):
-		board.ball_x = 2 - board.ball_x
-		board.velocity_x = (-1 * board.velocity_x) + random.uniform(-0.015, 0.015)
-		board.velocity_y = board.velocity_y + random.uniform(-0.03, 0.03)
-		board.reward = 1
+	if state.ball_x > 1 and (state.ball_y <= state.paddle_y and state.ball_y >= paddle_bottom):
+		state.ball_x = 2 - state.ball_x
+		state.velocity_x = (-1 * state.velocity_x) + random.uniform(-0.015, 0.015)
+		state.velocity_y = state.velocity_y + random.uniform(-0.03, 0.03)
+		state.reward = 1
 
-		if abs(board.velocity_x) <= 0.03:
+		if abs(state.velocity_x) <= 0.03:
 			print "VELOCITY_X ERROR"
 
 	#if ball passes paddle set state to TERMINAL
-	if board.ball_x > 1 and (board.ball_y > board.paddle_y and board.ball_y < paddle_bottom):
-		board.special = 1
-		board.reward = -1
+	if state.ball_x > 1 and (state.ball_y > state.paddle_y and state.ball_y < paddle_bottom):
+		state.special = 1
+		state.reward = -1
 
-	return (board, reward)
+	return (state, state.reward)
 
 #This function takes a game state and discretizes it
 def discretize(state): 
@@ -104,16 +119,16 @@ def discretize(state):
 
 	return state
 
-#Given a state and an action (nothing, UP, DOWN), this function moves the paddle
-def move_paddle(state, action): 
-	if state.special == 1: 
-		print "TERMINAL STATE. GAME OVER"
-		return None
+# #Given a state and an action (nothing, UP, DOWN), this function moves the paddle
+# def move_paddle(state, action): 
+# 	if state.special == 1: 
+# 		print "TERMINAL STATE. GAME OVER"
+# 		return None
 
-	if action!= 0 or action!=1 or action!= 2:
-		print "INVALID ACTION"
+# 	if action!= 0 or action!=1 or action!= 2:
+# 		print "INVALID ACTION"
 
-	state.paddle_y = state.paddle_y + state.actions[action]
+# 	state.paddle_y = state.paddle_y + state.actions[action]
 
 def maptoidx(state):
 	ten_thousand = state.ball_x * (10000)
@@ -128,7 +143,6 @@ def maptoidx(state):
 
 # This function returns the action you should take given a table and an index
 def exploration_policy(q_table, q_idx): 
-
 	if q_table[q_idx][0] == q_table[q_idx][1] == q_table[q_idx][2]:
 			a = random.randint(0,2)
 	else:
@@ -136,64 +150,82 @@ def exploration_policy(q_table, q_idx):
 
 	return a
 
-def qlearning_agent(alpha, gamma):
+def qlearning_agent(alpha_C, gamma):
 	# IN CASE OF BUGS IMPOSE ADDITIONAL VELOCITY BOUNDS
 	# Q table and seen table
-	q_table = [(0, 0, 0)] * 10369
-	N= [(0, 0, 0)] * 10369
+	# q_table = [(0, 0, 0)] * 10369
+	# N= [(0, 0, 0)] * 10369
+	global q_table 
+	global N 
+
 	reward = 0 
 	num_bounces = 0 
 	n = 0 # number iterations
 
 	#initialize board; board is current game state 
 	paddle_height = 0.2
-	board = gameState(0.5, 0.5, 0.03, 0.01, 0.5 - paddle_height/2)
+	
 
 	while n < 100000:
+		if n%1000 == 0:
+			print n
+			if n> 0:
+				print num_bounces/1000.0
+				num_bounces = 0
+
 		#observe current state and convert from continuous to discrete space
+		#start from scratch
+		board = gameState(0.5, 0.5, 0.03, 0.01, 0.5 - paddle_height/2)
 		discretized_board = discretize(board)
 
 		#Terminal state check
 		if discretized_board == None:
 			print "ERROR"
-			return
+			continue
 
 		if discretized_board.special == 1:
 			return None
+			continue 
+
 		else:
 			#choose an action based on exploration policy
 			q_idx = maptoidx(discretized_board)
 			a = exploration_policy(q_table, q_idx)
 
 			#increment number of times seen state action pair (s,a) and use the right alpha
-			if N[q_idx][a] == 0: 
+			if N[q_idx][a] == 0:
+				# temp_tuple = list(N[q_idx])
+				# temp_tuple[a] = 1
 				N[q_idx][a] = 1
 				alpha = 1.0
 			else:
 				N[q_idx][a] += 1
+				# temp_tuple = list(N[q_idx])
+				# temp_tuple[a] += 1
+				# N[q_idx] = temp_tuple
 				alpha = 1.0 * (C/(C + N[q_idx][a]))
 			
 			#given action and current state get successor state
-			successor_state1 = move_paddle(discretized_board, a) #original state with moved paddle 
-			successor_state2, reward= play_game(successor_state1) #final successor state
+			successor_state, reward= play_game(discretized_board, a) #final successor state
 
 			#update q-table with current state and successor state
-			new_idx = maptoidx(successor_state2)
+			new_idx = maptoidx(successor_state)
 			successor_action = exploration_policy(q_table, new_idx)
 			q_table[q_idx][a] = q_table[q_idx][a] + (alpha * (reward + (gamma * successor_action) - q_table[q_idx][a]))
 			
 			if reward > 0: 
 				num_bounces +=1
+				print "HELLO"
 
 			#update game state to next state
-			board = successor_state2
+			board = successor_state
 		
-		count +=1 
+		n+=1 
 	return num_bounces
 
 def main(): 
-	qlearning_agent()
+	bounces = qlearning_agent(100, 0.7)
 
 
 if __name__ == '__main__':
-	main(1, 1)
+	main()
